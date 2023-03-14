22797bd04882b1f9e658661b1de4d1cda836587e
bootsma <bootsma@gmail.com>
2023-03-14 00:12:36 -0400

    adjustments to evaluator.py

--- nnunet/evaluation/evaluator.py
+++ nnunet/evaluation/evaluator.py
@@ -172,9 +172,10 @@ class Evaluator:
         _funcs = {m: ALL_METRICS[m] for m in self.metrics + self.advanced_metrics}
         frames = inspect.getouterframes(inspect.currentframe())
         s_time = time.time()
+        print(f'Test: {test}')
         print(f'Starting Metrics: {s_time}, Advanced: {advanced}',flush=True)
         for metric in self.metrics:
-            print(f'Running Metric: {metric} at {time.time()}',flush=True)
+            #print(f'Running Metric: {metric} at {time.time()}',flush=True)
             for f in frames:
                 if metric in f[0].f_locals:
                     _funcs[metric] = f[0].f_locals[metric]
